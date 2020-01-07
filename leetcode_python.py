a33381e776dc94d41bd5cae7b2a068dc81f179d1
qiuhuiGithub <947466871@qq.com>
2020-01-07 23:16:24 +0800

    leetcode python版本

--- leetcode_python.py
+++ leetcode_python.py
@@ -233,3 +233,68 @@ def maxArea(height):
 
 
 # 42.接雨水
+# def trap(height):  # 栈实现
+#     ans, current = 0, 0
+#     stack = []
+#     while current < len(height):
+#         while stack and height[current] > height[stack[-1]]:
+#             top = stack.pop()
+#             if not stack:
+#                 break
+#             distance = current - stack[-1] - 1
+#             bounded_height = min(height[current], height[stack[-1]]) - height[top]
+#             ans += distance * bounded_height
+#         stack.append(current)
+#         current += 1
+#     return ans
+
+def trap(height):
+    left, right = 0, len(height) - 1
+    ans = 0
+    left_max, right_max = 0, 0
+    while left < right:
+        if height[left] < height[right]:
+            if height[left] >= left_max:
+                left_max = height[left]
+            else:
+                ans += left_max - height[left]
+            left += 1
+        else:
+            if height[right] >= right_max:
+                right_max = height[right]
+            else:
+                ans += right_max - height[right]
+            right -= 1
+    return ans
+
+
+# print(trap([2, 1, 0, 1, 3, 2, 1,2]))
+
+# 43. 字符串相乘
+def multiply(num1, num2):
+    ans = ""
+    for i in range(len(num2) - 1, -1, -1):
+        res = int(num2[i])
+        mul = res * int(num1) * (10 ** (len(num2) - i - 1))
+        ans = addStrings(ans, str(mul))
+    return ans
+
+
+# 415.字符串相加
+def addStrings(num1, num2):
+    ans = ""
+    carry = 0
+    i, j = len(num1) - 1, len(num2) - 1
+    while i >= 0 or j >= 0 or carry > 0:
+        x = int(num1[i]) if i >= 0 else 0
+        y = int(num2[j]) if j >= 0 else 0
+        sum = x + y + carry
+        carry = sum // 10
+        sum = sum % 10
+        ans += str(sum)
+        i -= 1
+        j -= 1
+    return ans[::-1]
+
+# print(addStrings('123', '956'))
+# print(multiply('123', '456'))
